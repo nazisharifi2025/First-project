fd3e06ae25a085c25e32ba7bbac7be2cc12909ce
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-20 13:20:59 +0430

    l

---
+++ poblic/connection.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "store"
+$conniction = new mysqli($servername,$username,$password,$dbName);
+                                                                                                                                                                                                                
+?>
