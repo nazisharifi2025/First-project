75a449edbad9d30c0f1a2cdc49ca52007042a696
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-23 17:26:12 +0430

    l

---
+++ poblic/form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../src/output.css">
+</head>
+<body>
+    <div class="">
+        <form action="">
+            <div>
+                <label for="">ID</label>
+                <input type="number">
+            </div>
+            <div>
+                <label for="">NAME</label>
+                <input type="text">
+            </div>
+            <div>
+                <label for="">PRODUCT</label>
+                <input type="text">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
