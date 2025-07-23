14b8ff3d5556b8f7bdb9b280cf19b9623796f01d
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-23 16:46:24 +0430

    l

--- poblic/creatTable.php
+++ poblic/creatTable.php
@@ -2,5 +2,5 @@
 include "connection.php";
 $commind = "CREATE TABLE product(id INT PRIMARY KEY , name VARCHAR(50), PRICE INT NOT NULL)";
 
-$connection-> query ($commind);
+$connection-> query($commind);
 ?>
