e2fee93291e78cf2bb3de25fca03be5f0ac0057c
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-25 19:28:09 +0430

    l

--- poblic/form.php
+++ poblic/form.php
@@ -9,7 +9,7 @@
 <body>
     <div class="h-screen w-full gap-6  flex justify-center flex-col items-center overflow-y-scroll">
         <h1 class="font-bold text-4xl text-center uppercase">Add New Car</h1>
-        <form class="h-[60%] w-[40%] p-4 shadow-xl rounded-2xl  shadow-black border gap-4    justify-between flex flex-col " action=<?php $_SERVER["PHP_SELF"] ?> >
+        <form class="h-[80%] w-[40%] p-4 shadow-xl rounded-2xl  shadow-black border gap-4    justify-between flex flex-col " action=<?php $_SERVER["PHP_SELF"] ?> >
             <div >
                 <label for="" class="font-bold">ID:</label>
                 <input type="number" name="id" class="py-2 w-full border rounded-xl outline-0">
@@ -19,13 +19,12 @@
                 <input type="text" name="name" class="py-2 w-full border rounded-xl outline-0">
             </div>
             <div>
-                <label for=""  class="font-bold">BRAND:</label>
-                <input type="text" name="product" class="py-2 w-full border rounded-xl outline-0">
+                <label for=""  class="font-bold">IMGURL:</label>
+                <input type="file" name="product" class="py-2 w-full border rounded-xl outline-0">
             </div>
             <div>
                 <label for=""  class="font-bold">MODEL:</label>
                 <input type="text" name="product" class="py-2 w-full border rounded-xl outline-0">
-            </div>
             </div>
             <div>
                 <label for=""  class="font-bold">PRICE:</label>
