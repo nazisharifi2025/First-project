7fca060c97ab87ae9b5f52bc0bd3e825f065c5d9
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-25 19:50:21 +0430

    l

--- poblic/read.php
+++ poblic/read.php
@@ -10,6 +10,7 @@
     <div class="h-screen w-full p-6 flex justify-between flex-wrap overflow-y-scroll">
         <div class="h-[350px] w-[250px] rounded-2xl shadow-md shadow-gray-700 flex justify-center items-center"></div>
         <div class="h-[350px] w-[250px] rounded-2xl shadow-md shadow-gray-700 flex justify-center items-center"></div>
+        <div class="h-[350px] w-[250px] rounded-2xl shadow-md shadow-gray-700 flex justify-center items-center"></div>
     </div>
 </body>
 </html>
