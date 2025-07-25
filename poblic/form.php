d929375c2bba8607aefcacab37ff0fff9daac3a9
nazisharifi2025 <banonazisharifi@gmail.com>
2025-07-25 19:41:52 +0430

    l

--- poblic/form.php
+++ poblic/form.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div class="h-screen w-full gap-6  flex justify-center flex-col items-center overflow-y-scroll">
-        <h1 class="font-bold text-4xl text-center uppercase">Add New Car</h1>
+        <h1 class="font-bold text-4xl text-center uppercase shadow-md shadow-gray-700">Add New Car</h1>
         <form class="h-[80%] w-[40%] p-4 shadow-xl rounded-2xl  shadow-black border gap-4    justify-between flex flex-col " action=<?php $_SERVER["PHP_SELF"] ?> >
             <div >
                 <label for="" class="font-bold">ID:</label>
@@ -30,7 +30,7 @@
                 <label for=""  class="font-bold">PRICE:</label>
                 <input type="text" name="product" class="py-2 w-full border rounded-xl outline-0">
             </div>
-            <button class="py-2 px-3 w-[40%] bg-gray-300 shadow-md shadow-gray-800 rounded-xl font-bold mx-auto hover:bg-white hover:shadow-xl transition-all">SUBMIT</button>
+            <button class="py-2 px-3 w-[40%] bg-gray-300 shadow-md shadow-gray-800 rounded-xl font-bold mx-auto hover:bg-white hover:shadow-xl transition-all delay-300 ">SUBMIT</button>
         </form>
     </div>
 </body>
